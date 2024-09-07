7d15aa7863a39202f3a0d21309a4067912282a14
luanrodrigueseduardo <167102015+luanrodrigueseduardo@users.noreply.github.com>
2024-09-06 21:37:12 -0300

    criação da classe pedido

--- Pedido.cs
+++ Pedido.cs
@@ -3,8 +3,35 @@ using System.Collections.Generic;
 
 public class Pedido
 {
-	private string nome;
-	private double preco;
 
-}
+	private List<Produto> produtos;
+	private Cliente cliente;
+	public Pedido (Cliente cliente)
+	
+	{
+		this.cliente = cliente;
+		this.produtos = new List<Produto> ;
+	}
+	
+	public void AdicionarProduto(Produto produto)
+	{
+		produtos.Add(produto);
+	}
 
+	public object BuscarPorNome(string nome) 
+	{
+	
+		
+			if (produto.GetNome() == nome)
+			{
+				return produto;
+			}
+		
+
+		if (cliente.GetNome() == nome)
+		{
+			return cliente;
+		}
+
+		return null;
+	}
--- Produto.cs
+++ Produto.cs
@@ -10,7 +10,7 @@ public class Produto
     public Produto(string nome, double preco)
     {
         this.nome = nome;
-        this.preco = preco;
+       this.preco = preco;
     }
 
     public string GetNome()
